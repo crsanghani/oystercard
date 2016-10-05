d74b343bfba2cd14ec0d8a6996dd3e42c2969851
Chetan Sanghani <crsanghani@gmail.com>
2016-10-05 10:48:24 +0100

    wrote a touch_out test for journey

---
+++ lib/journey.rb
@@ -0,0 +1,13 @@
+require 'oystercard'
+
+class Journey
+
+
+  def initialize
+    @in_journey =  true
+  end
+
+  def in_journey?
+    @in_journey
+  end
+end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -1,15 +1,21 @@
 require 'journey'
 
-let(:oystercard) {Oystercard.new}
-
 describe Journey do
+
+  let(:oystercard) {Oystercard.new}
+  let(:entry_station) {Station.new(name: "Mayfair", zone: "Zone 1")}
+  let(:exit_station) {Station.new(name: "Kings Cross", zone: "Zone 2")}
+
   before(:each) do
-    oystercard.top_up(MAXIMUM_BALANCE)
+    oystercard.top_up(Oystercard::MAXIMUM_BALANCE)
   end
 
   it "should start a journey" do
-    oystercard.touch_in
+    oystercard.touch_in(entry_station)
     expect(subject).to be_in_journey
   end
 
+  it "should end a journey" do
+    oystercard.touch_out(exit_station)
+    expect(subject).not_to be_in_journey
 end
