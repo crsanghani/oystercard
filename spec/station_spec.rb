850009bd85ea56b0f0961349cfc96fb7608ecd99
Chetan Sanghani <crsanghani@gmail.com>
2016-10-05 10:04:42 +0100

    challenge 13 station tests written

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,5 +2,13 @@ require 'station'
 
 describe Station do
 
-  
+  subject { described_class.new(name: "Kings Cross", zone: 2) }
+
+  it 'has a name' do
+    expect(subject.name).to eq("Kings Cross")
+  end
+
+  it 'has a name' do
+    expect(subject.zone).to eq(2)
+  end
 end
